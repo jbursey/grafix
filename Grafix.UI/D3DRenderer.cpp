1650b60d8ae3641672c105bbbe4e24c58423daad
Jeffrey Bursey <jeffrey.bursey8806@live.com>
2019-09-12 14:27:19 -0500

    depth stencil view brush up

--- Grafix.UI/D3DRenderer.cpp
+++ Grafix.UI/D3DRenderer.cpp
@@ -38,32 +38,37 @@ void D3DRenderer::Init(HWND handle, int width, int height)
 	auto d3dresult = D3D11CreateDeviceAndSwapChain(_dxgiAdapter, D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_UNKNOWN, 0, deviceflags, levels, 1, D3D11_SDK_VERSION, &swapDesc, &_dxgiSwapChain, &_device, 0, &_context);
 
 	//--create render target view	
-	//ID3D11Texture2D* rtvTexture;
-	//D3D11_TEXTURE2D_DESC texDesc;
-	//texDesc.ArraySize = 1;
-	//texDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_RENDER_TARGET;
-	//texDesc.CPUAccessFlags = D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_READ;
-	//texDesc.Format = DXGI_FORMAT::DXGI_FORMAT_B8G8R8A8_UNORM;
-	//texDesc.Height = height;
-	//texDesc.MipLevels = 0;
-	//texDesc.MiscFlags = 0;
-	//texDesc.SampleDesc.Count = 1;
-	//texDesc.SampleDesc.Quality = 0;
-	//texDesc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
-	//texDesc.Width = width;
-	//auto rtvTextureResult = _device->CreateTexture2D(&texDesc, 0, &rtvTexture);	
-
 	ID3D11Texture2D* backBuffer;
 	auto backBufferGetResult = _dxgiSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)& backBuffer);
-	auto rtvResult = _device->CreateRenderTargetView(backBuffer, nullptr, &_rtv);	
+	auto rtvResult = _device->CreateRenderTargetView(backBuffer, nullptr, &_rtv);
+	_context->OMSetRenderTargets(1, &_rtv, 0);
 
-	////--create depth stencil view
-	//D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc;	
-	//dsvDesc.Format = DXGI_FORMAT::DXGI_FORMAT_D24_UNORM_S8_UINT;
-	//dsvDesc.ViewDimension = D3D11_DSV_DIMENSION::D3D11_DSV_DIMENSION_TEXTURE2D;
+	//--create depth stencil view
+	D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc;	
+	ZeroMemory(&dsvDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+	dsvDesc.Format = DXGI_FORMAT::DXGI_FORMAT_D24_UNORM_S8_UINT;
+	dsvDesc.ViewDimension = D3D11_DSV_DIMENSION::D3D11_DSV_DIMENSION_TEXTURE2D;	
+	dsvDesc.Texture2D.MipSlice = 0;
 
-	//auto dsvResult = _device->CreateDepthStencilView(0, &dsvDesc, &_dsv);
+	ID3D11Texture2D* depthBuffer;
+	D3D11_TEXTURE2D_DESC texDesc;
+	ZeroMemory(&texDesc, sizeof(texDesc));
+	texDesc.ArraySize = 1;	
+	texDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_DEPTH_STENCIL;
+	//texDesc.CPUAccessFlags = D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_READ;
+	texDesc.CPUAccessFlags = 0;
+	texDesc.Format = DXGI_FORMAT::DXGI_FORMAT_D24_UNORM_S8_UINT;
+	texDesc.Height = height;
+	texDesc.MipLevels = 1;	
+	texDesc.MiscFlags = 0;
+	texDesc.SampleDesc.Count = 1;
+	texDesc.SampleDesc.Quality = 0;
+	texDesc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
+	texDesc.Width = width;
+	auto depthBufferResult = _device->CreateTexture2D(&texDesc, 0, &depthBuffer);
 
+	auto dsvResult = _device->CreateDepthStencilView(depthBuffer, &dsvDesc, &_dsv);	
+	
 	//--create viewport
 	D3D11_VIEWPORT viewport;
 	viewport.Height = height;
@@ -99,7 +104,7 @@ void D3DRenderer::Render()
 	color[1] = green;
 	color[2] = blue;
 	color[3] = 255;
-	//_context->ClearDepthStencilView(0, D3D11_CLEAR_FLAG::D3D11_CLEAR_DEPTH, 1.0, 0);	
+	_context->ClearDepthStencilView(_dsv, D3D11_CLEAR_FLAG::D3D11_CLEAR_DEPTH, 1.0, 0);	
 	_context->ClearRenderTargetView(_rtv, &color[0]);
 
 	_dxgiSwapChain->Present(0, 0);
